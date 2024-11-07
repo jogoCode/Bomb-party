8c703ef091c148993c44d3f7edb1d912a6a56360
Frederheat <keplerfrederic@gmail.com>
2024-11-07 11:58:20 +0400

    BombTagManager+PlayerBombTag+

--- Assets/Scripts/BombsScripts/BombTag.cs
+++ Assets/Scripts/BombsScripts/BombTag.cs
@@ -4,25 +4,4 @@ using UnityEngine;
 
 public class BombTag : MonoBehaviour
 {
-    public float _bombTimer = 10;
-    public bool _boom = false;
-
-    void Update()
-    {
-        BombTimer();
-
-    }
-    void BombTimer()
-    {
-        if (_bombTimer > 0)
-        {
-            _bombTimer = Mathf.Clamp(_bombTimer, 0, 10) - Time.deltaTime; // diminue le titer de la bombe au fine du temps 
-        }
-        else
-        {
-            _boom = true;
-            //Debug.Log("Timer of Bombe is = to : " + _bombTimer);
-            //Debug.Log("Boom !");
-        }
-    }
 }
