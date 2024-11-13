3606bca7a2c49b39625a2124c201fc2459792533
Frederheat <keplerfrederic@gmail.com>
2024-11-13 21:38:38 +0400

    Fix Stun + bombshower and BombTag can Switch scene

--- Assets/scripts/Managers/BombShowerManager.cs
+++ Assets/scripts/Managers/BombShowerManager.cs
@@ -12,25 +12,34 @@ public class BombShowerManager : MonoBehaviour
     public float _time;
     float _baseTime;
     bool _isTimerRunning;
+    FeedBackManager _fbM;
+    bool _finish;
 
     private void Start()
     {
+        //foreach (PlayerController i in GameManager.Instance.GetPlayerManager().GetPlayerList())
+        //{
+        //    i.gameObject.SetActive(true);
+        //}
+        
+        _fbM = FeedBackManager.Instance;
         m_gameManager = GameManager.Instance;
-        _scoreManager = GameManager.Instance.GetScoreManager();
+        _scoreManager = m_gameManager.GetScoreManager();
         _baseTime = _time;
         _isTimerRunning = true;
+        _finish = false;
     }
 
     public void PlayerEliminated(PlayerController player)
     {
         _scoreManager.AddPlayerToList(player, _scoreManager.Bonus);
+        _fbM.InstantiateParticle(_fbM.m_explosionVfx, player.gameObject.transform.position, player.gameObject.transform.rotation);
+        HasAWinner();
     }
 
     private void Update()
     {
         UpdateTimer();
-        
-
     }
 
     void UpdateTimer()
@@ -48,9 +57,23 @@ public class BombShowerManager : MonoBehaviour
                 {
                     _scoreManager.OneWin();
                     _time = _baseTime;
-                    m_gameManager.GetPartyManager().ChangeMiniGame();
+                    
                 }
+                
+                _finish = true;
             }
+        }
+    }
+    public void HasAWinner()
+    {
+        if (m_gameManager.GetPlayerManager().GetActivePlayers().Count <= 1)
+        {
+            _time = 0;
+            m_gameManager.GetPartyManager().ChangeMiniGame();
+        }
+        if (_finish)
+        {
+            _finish = false;
         }
     }
 }
