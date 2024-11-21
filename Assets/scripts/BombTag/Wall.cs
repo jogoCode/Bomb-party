70dc78cc617ac642482a8d72504e4e97d4b5e617
SpecterWolf974 <163996201+SpecterWolf974@users.noreply.github.com>
2024-11-21 23:16:05 +0400

    dernier truc de fin de soirée

--- Assets/scripts/BombTag/Wall.cs
+++ Assets/scripts/BombTag/Wall.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Wall : MonoBehaviour
 {
     public BombTagManager _bombtagM;
+    [SerializeField] GameObject _contactVFX;
 
     private void Start()
     {
@@ -17,6 +18,8 @@ public class Wall : MonoBehaviour
         {
             Debug.Log("ntm");
             PlayerController player = other.gameObject.GetComponent<PlayerController>();
+            Instantiate(_contactVFX, other.transform.position, other.transform.rotation);
+            SoundManager.Instance.PlaySFX("PushSurPlayer");
             other.gameObject.SetActive(false);
 
             ScoreManager score = GameManager.Instance.GetScoreManager();
